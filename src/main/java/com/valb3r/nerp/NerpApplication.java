43e5fc12b7c38b5a1c9c1752a862ba4405fd466a
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-11 11:56:53 +0300

    Intitialize project

---
+++ src/main/java/com/valb3r/nerp/NerpApplication.java
@@ -0,0 +1,13 @@
+package com.valb3r.nerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valb3r/nerp/NerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valb3r.nerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
